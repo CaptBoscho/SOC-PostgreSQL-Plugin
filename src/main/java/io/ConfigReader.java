333876cfada4f2dad0ae657522b054d823a2fe4a
Derek Argueta <darguetap@gmail.com>
2016-04-09 23:29:04 -0600

    fix shiz

--- src/main/java/io/ConfigReader.java
+++ src/main/java/io/ConfigReader.java
@@ -10,21 +10,27 @@ public class ConfigReader {
 
     public static Properties readConfig() {
         Properties properties = new Properties();
-        InputStream inputStream = null;
-        try {
-            inputStream = new FileInputStream(new File("db.properties"));
-            properties.load(inputStream);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if(inputStream != null) {
-                try {
-                    inputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+//        InputStream inputStream = null;
+//        try {
+//            inputStream = new FileInputStream(new File("db.properties"));
+//            properties.load(inputStream);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//            if(inputStream != null) {
+//                try {
+//                    inputStream.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+
+        properties.setProperty("dbname", "test");
+        properties.setProperty("username", "soc");
+        properties.setProperty("password", "settlersofswag");
+        properties.setProperty("hostname", "soc-test.cinp8shdrsvf.us-east-1.rds.amazonaws.com");
+        properties.setProperty("port", "5432");
 
         return properties;
     }
